315ebf330e26bb1c9cfcc4f852bd9520923f2d3d
Blacksilver42 <blacksilverck35@gmail.com>
2017-11-30 18:46:29 +0000

    use this to package everything into min.js

---
+++ minify.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "" > min.js
+cat js/animateRotate.js >> min.js
+cat js/data.js >> min.js
+cat js/dropmenu.js >> min.js
+cat js/hiring.js >> min.js
+cat js/stat.js >> min.js
+cat js/upgrades.js >> min.js
+cat js/science.js >> min.js
+
